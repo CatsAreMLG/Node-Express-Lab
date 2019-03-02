d4e7b8a056a8b77a57d2d7db61eb41b119f60cb2
CatsAreMLG <joshuadgon@gmail.com>
2019-03-02 11:43:10 -0600

    Add post handler

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -26,4 +26,20 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  if (req.body && req.body.title && req.body.content)
+    try {
+      const post = await Posts.insert(req.body)
+      res.status(201).json({ ...post, ...req.body })
+    } catch (error) {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      })
+    }
+  else
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide title and contents for the post.' })
+})
+
 module.exports = router
