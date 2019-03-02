294a3324acc57e853aa4e186526458f4ed3fe24e
CatsAreMLG <joshuadgon@gmail.com>
2019-03-02 11:54:28 -0600

    Add delete handler

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -9,20 +9,24 @@ router.get('/', async (req, res) => {
     const posts = await Posts.find()
     res.status(200).json(posts)
   } catch (error) {
-    res.status(500).json({
-      message: 'Error retrieving the posts'
-    })
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' })
   }
 })
 
 router.get('/:id', async (req, res) => {
   try {
     const post = await Posts.findById(req.params.id)
-    res.status(200).json(post)
+    post.length
+      ? res.status(200).json(post)
+      : res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' })
   } catch (error) {
-    res.status(500).json({
-      message: 'Error retrieving the posts'
-    })
+    res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' })
   }
 })
 
@@ -40,6 +44,19 @@ router.post('/', async (req, res) => {
     res
       .status(400)
       .json({ errorMessage: 'Please provide title and contents for the post.' })
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Posts.remove(req.params.id)
+    post
+      ? res.status(200).json(post)
+      : res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' })
+  } catch (error) {
+    res.status(500).json({ error: 'The post could not be removed' })
+  }
 })
 
 module.exports = router
