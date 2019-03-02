2375ddd0e201f1bdc46d80a5ece91b5264b251ae
CatsAreMLG <joshuadgon@gmail.com>
2019-03-02 11:59:59 -0600

    Add put handler

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  if (req.body && req.body.title && req.body.content)
+  if (req.body && req.body.title && req.body.contents)
     try {
       const post = await Posts.insert(req.body)
       res.status(201).json({ ...post, ...req.body })
@@ -57,6 +57,26 @@ router.delete('/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'The post could not be removed' })
   }
+})
+
+router.put('/:id', async (req, res) => {
+  if (req.body && req.body.title && req.body.contents)
+    try {
+      const post = await Posts.update(req.params.id, req.body)
+      post
+        ? res.status(200).json({ ...post, ...req.body })
+        : res
+            .status(404)
+            .json({ message: 'The post with the specified ID does not exist.' })
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' })
+    }
+  else
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide title and contents for the post.' })
 })
 
 module.exports = router
